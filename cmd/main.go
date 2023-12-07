939895741e34dad147a55a708438f6bc08366074
Majid ali <majidali37406@gmail.com>
2023-12-07 09:48:10 +0500

    gin chat routes

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,13 @@ import (
 	"fmt"
 	"net/http"
 
+	chating "github.com/Majidali343/web-sockets-golang/internal/handlers"
 	"github.com/gorilla/websocket"
+
+	tokens "github.com/Majidali343/web-sockets-golang/internal/Middleware"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 var upgrader = websocket.Upgrader{
@@ -21,24 +27,41 @@ type Message struct {
 var clients = make(map[*websocket.Conn]bool)
 var broadcast = make(chan Message)
 
-func main() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/ws", handleConnections)
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/", homePage)
+	r.GET("/ws", handleConnections)
 
 	go handleMessages()
 
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "hello"})
+	})
+
+	r.POST("/login", chating.Login)
+	r.POST("/register", chating.Register)
+	r.POST("/refresh", tokens.RefreshTokenHandler)
+
+	return r
+}
+
+func main() {
+	router := SetupRouter()
+	router.Run(":8080")
+
 	fmt.Println("Server started on :8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic("Error starting server: " + err.Error())
-	}
+
 }
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Chat Room!")
+func homePage(c *gin.Context) {
+	w := c.Writer
+	fmt.Fprintf(w, "Chat room is working!")
 }
 
-func handleConnections(w http.ResponseWriter, r *http.Request) {
+func handleConnections(c *gin.Context) {
+	w := c.Writer
+	r := c.Request
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println(err)
---
+++ internal/handlers/LoginRegister.go
@@ -0,0 +1,111 @@
+package chat
+
+import (
+	"net/http"
+	dbconnect "github.com/Majidali343/web-sockets-golang/internal/db"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+
+	tokens "github.com/Majidali343/web-sockets-golang/internal/Middleware"
+)
+
+// const SecretKey = "Majid ali"
+
+// User represents the structure of a user.
+type User struct {
+	gorm.Model
+	Email    string `json:"email" gorm:"not null;unique"`
+	Password string `json:"password" gorm:"not null"`
+}
+
+type MyUser struct {
+	Email    string `json:"email" `
+	Password string `json:"password"`
+}
+
+type logeduser struct {
+	LoggedUserID uint   `json:"loggeduserid"`
+	Adminemail   string `json:"email" `
+}
+
+var LogedUser logeduser
+
+func Register(c *gin.Context) {
+	var user MyUser
+
+	// Parse JSON request into MyUser struct
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Connect to the database
+	db := dbconnect.Dbconnection()
+	defer db.Close()
+	db.AutoMigrate(&User{})
+
+	var existingUser User
+	if err := db.Where("email = ?", user.Email).First(&existingUser).Error; err == nil {
+		// User already exists
+		c.JSON(http.StatusConflict, gin.H{"error": "User with this email already exists"})
+		return
+	}
+	// Create a new user in the database
+	var newUser User
+	newUser.Email = user.Email
+	newUser.Password = user.Password
+
+	if err := db.Create(&newUser).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to register user", "details": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User registered successfully"})
+}
+
+func Login(c *gin.Context) {
+	var user MyUser
+
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
+		return
+	}
+
+	var dbUser User
+
+	// Connect to the database
+	db := dbconnect.Dbconnection()
+	defer db.Close()
+
+	// Check if the user with the provided email exists
+	if err := db.Where("email = ?", user.Email).First(&dbUser).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	// Verify the password
+	if dbUser.Password != user.Password {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	accessToken, err := tokens.GenerateAccessToken()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+
+	refreshToken, err := tokens.GenerateRefreshToken()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+
+	LogedUser.LoggedUserID = dbUser.ID
+
+	// Respond with user ID and token
+	c.JSON(http.StatusOK, gin.H{"user_id": dbUser.ID, "access_token": accessToken,
+		"refresh_token": refreshToken})
+}
